6396b7da43cb3043c6bfe3eab4367b491ed41a04
Tejas <tejas14all@gmail.com>
2021-07-24 18:02:59 +0530

    avl

--- avl.c
+++ avl.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-
+#include<stdlib.h>
 struct Node{
 
     int key;
@@ -22,12 +22,12 @@ struct Node* createNode(int key){
 
 int max (struct Node *left,struct Node *right){ //null , 60
     int l,r;
-    if(left == null){
+    if(left == NULL){
         l=0;
     }else{
         l = left->height;
     }
-    if(right == null){
+    if(right ==NULL){
         r=0;
     }else{
         r = right->height;// 1
@@ -37,8 +37,66 @@ int max (struct Node *left,struct Node *right){ //null , 60
 
 }
 
-struct Node* insert(struct Node *root,int key){ //50,55 | 60,55 | null,55
+
+int getBf(struct Node *root){
+    int l,r;
+
+    if(root->left == NULL){
+        l=0;
+    }else{
+        l = root->left->height;
+    }
+
+
+    if(root->right == NULL){
+        r=0;
+    }else{
+        r = root->right->height;// 1
+    }
+
+    return l-r;
+}
+
+struct Node* getParent(struct Node *root,int key){
+
+}
+
+struct Node* leftRotate(struct Node *x){
+
+        struct Node *y = x->right;
+        struct Node *yl = y->left;
+
+        y->left = x;
+        x->right=yl;
+
+        x->height = 1 + max( x->left,x->right );
+        y->height = 1 + max( y->left,y->right );
+
+}
+
+
+
+struct Node* rightRotate(struct Node *x){
+
+        struct Node *y = x->left;
+        struct Node *yr = y->right;
+
+        y->right = x;
+        x->left=yr;
+
+        x->height = 1 + max( x->left,x->right );
+        y->height = 1 + max( y->left,y->right );
+
+}
+
+
+
+
+struct Node* insert(struct Node *root,int key){ //50,70   60,70
+    int balance;
+    struct Node *parent;
     if(root == NULL){
+
         return createNode(key);//height = 1
     }
 
@@ -50,24 +108,73 @@ struct Node* insert(struct Node *root,int key){ //50,55 | 60,55 | null,55
     else if(key > root->key){
             //right
             root->right = insert(root->right,key); // 30->right 60->right  70->right [80
-            //50->right = insert(60,65)
-
+            //50->right = insert(60,70)
+            //60->right = 70
 
     }else{
+        printf("\nInvalid");
         return root;
     }
 
     root->height = 1 + max( root->left,root->right );
-
     //balance factor
-};
 
+
+    balance = getBf(root);//50 -> -1
+
+            //R
+   // parent  = getParent(root,key);
+
+    if( balance < -1 && key > root->right->key){
+        //RR
+        printf("RR");
+        return leftRotate(root);
+    }else if(balance < -1 && key < root->right->key){
+        //RL
+        printf("RL");
+        root->right= rightRotate(root);
+        return leftRotate(root);
+    }else if(balance > 1 && key < parent->key){
+        //LL
+        printf("LL");
+        return rightRotate(root);
+    }else if(balance >  1 && key > root->left->key){
+        //LR
+        printf("LR");
+        root -> left = leftRotate(root);
+        return rightRotate(root);
+    }else{
+        printf("\nBal = %d for %d with %d ",balance,root->key,key);
+    }
+
+    return root;
+};
+void preOrder(struct Node *root){
+
+    if(root != NULL){
+        printf(" %d",root->key);
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
 
 void main(){
     struct Node *root = NULL;
-
+    /*
      root  = insert(root,50); // root->50
      root = insert(root,60); //
      root = insert(root,70); //root->50 , 70
      root = insert(root,55);//root->50 , 55
+    */
+
+        root=insert(root,50);
+        root= insert(root,45);
+
+             root= insert(root,60);
+             root= insert(root,55);
+            root=  insert(root,70);
+             root= insert(root,65);
+
+            printf("\n\n");
+            preOrder(root);
 }
